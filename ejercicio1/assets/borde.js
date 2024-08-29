3129fc3c55b7caf70b71568b162332f0e0af64e2
Tamara <tamara.zapatagomez@gmail.com>
2024-08-28 21:30:29 -0400

    correcciones ejercicio 1

--- ejercicio1/assets/borde.js
+++ ejercicio1/assets/borde.js
@@ -1,16 +1,21 @@
 let image = document.getElementById("clickeable-img");
 let currentColor = image.style.borderColor || window.getComputedStyle(image).borderColor;
+let currentBorder = image.style.border || window.getComputedStyle(image).border;
+console.log(currentBorder)
+console.log(currentColor)
+
 
 function changeBorder() { 
-    if (currentColor === "" || currentColor === "black" || currentColor === "rgb(0, 0, 0)") {
+if (currentBorder === "0px none rgba(0, 0, 0, 0)" && currentColor === "rgba(0, 0, 0, 0)" || image.style.borderColor  == "initial") {
         image.style.borderColor = "red";
-        currentColor = "red"; 
-        console.log(image.style.borderColor);
+        image.style.border = "2px solid red"
+        currentColor = "red"
+        console.log("Border: " + image.style.border  + " | Border color: " + image.style.borderColor )
+        console.log(image.style.border == "2px solid red" )
     } 
-    else if (currentColor === "red" || currentColor === "rgb(255, 0, 0)") {
-        image.style.borderColor = ""; 
-        currentColor = ""; 
-        console.log(image.style.borderColor);
+    else if (image.style.border == "2px solid red") {
+        image.style.borderColor = "transparent"; 
+        image.style.border = "0px"
+        console.log("Border: " + image.style.border  + " | Border color: " + image.style.borderColor )
     }
-    
 }
